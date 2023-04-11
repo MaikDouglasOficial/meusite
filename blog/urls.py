b36aa37109fd016480ebcd4b1cd7e69b6e16b93c
MaikDouglasOficial <maik.ferreira@estudante.ifto.edu.br>
2023-04-10 22:49:01 -0300

    Mudança de Layout, adicionando pagina de rascunho e login, e adicionando botão de excluir post e publicar.

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('rascunhos/', views.post_draft_list, name='post_draft_list'),
     path('sobre/', views.sobre, name='sobre'),
     path('sugestao/', views.sugestao, name='sugestao'),
+    path('post/<int:pk>/publicar/', views.post_publicar, name='post_publicar'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            if 'save_draft' in request.POST:
+                post.published_date = None  # define o post como rascunho
+                messages.success(request, 'Post salvo como rascunho.')
+            else:
+                post.published_date = timezone.now()
+                messages.success(request, 'Post publicado com sucesso.')
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -29,6 +39,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -36,15 +47,32 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def sobre(request):
     return render(request, 'blog/sobre.html')
 
+
 def sugestao(request):
     return render(request, 'blog/sugestao.html', {})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+@login_required
+def post_publicar(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publicar()
+    return redirect('post_detail', pk=post.pk)
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
