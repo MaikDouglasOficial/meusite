e0c201f3aab21b1b53c917dda1773d306ee7c564
MaikDouglasOficial <maik.ferreira@estudante.ifto.edu.br>
2023-04-14 19:59:24 -0300

    Teste de compatibilidade.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,4 @@
-from ckeditor.fields import RichTextField
+
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    text = RichTextField()
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='blog/media/media', blank=True, null=True)
